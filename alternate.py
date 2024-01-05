c2bb03901619ae04885af90c941d796120e58e76
Josh Gray <joshgray91@gmail.com>
2024-01-05 21:50:19 +0000

    implement capitalize* functions and testing

--- alternate.py
+++ alternate.py
@@ -5,17 +5,30 @@ Function takes a string, and depending on the users input it will:
  1) Upper and lower-case alternating words
 """
 
-def app():
+def get_input():
+    """Simple function to return user input."""
+    return input("please enter a string: ")
+
+def app() -> None:
     print("initial app function")
+    user_str = get_input()
+    capitalize_letters(user_str)
+    print(capitalize_words(user_str))
 
-def test_func(x: float) -> float:
-    print(x + 1)
-    return x + 1
 
+def capitalize_words(user_str: str) -> str:
+    """Takes a string provided by user and fully capitalises every other word before returning."""
+    user_list = user_str.split()
+    return " ".join([word.upper() if i % 2 else word.lower() for i, word in enumerate(user_list)])
+   
+
+def capitalize_letters(user_str: str) -> str:
+    """Takes a string provided by user and capitalises every other letter before returning."""
+    
+    return "".join([word.upper() if i % 2 else word.lower() for i, word in enumerate(user_str)])
 
 print(f"{"="*10}alternate.py{"="*10}\n")
 
-app()
-test_func(5)
+# app()
 
 print(f"\n{"="*10}alternate.py END{"="*10}")
--- alternate_test.py
+++ alternate_test.py
@@ -1,4 +1,59 @@
-import alternate
+"""testing suite for alternate.py"""
+import pytest
+from alternate import capitalize_words, capitalize_letters, get_input
+from unittest.mock import patch
+from unittest import TestCase
 
-def test_answer():
-    assert alternate.test_func(3) == 4
+
+def answer():
+    ans = get_input()
+    if ans == 'yes':
+        return 'you entered yes'
+    if ans == 'no':
+        return 'you entered no'
+    
+class Test(TestCase):
+
+    # get_input will return 'yes' during this test
+    @patch('builtins.input', return_value='yes')
+    def test_answer_yes(self, input):
+        self.assertEqual(answer(), 'you entered yes')
+
+    @patch('builtins.input', return_value='no')
+    def test_answer_no(self, input):
+        self.assertEqual(answer(), 'you entered no')
+
+def test_capitalize_words_returns_string():
+    """ensures that capitalize_words returns a string"""
+    assert isinstance(capitalize_words("test"), str)
+
+@pytest.mark.parametrize(
+    ("input_str", "expected"),
+    (
+        ("this is a test", "this IS a TEST"),
+        ("THIS IS A TEST", "this IS a TEST"),
+        ("tHe QuiCk FOX jumpED OVEr the LAzy DoG","the QUICK fox JUMPED over THE lazy DOG"),
+    )
+)
+
+def test_capitalize_words(input_str, expected):
+    "test that capitalize_words returns an expected value"
+    assert capitalize_words(input_str)  ==  expected
+
+def test_capitalise_letters_returns_string():
+    """ensures that capitalize_letters returns a string"""
+    assert isinstance(capitalize_letters("test"), str)
+
+@pytest.mark.parametrize(
+    ("input_str", "expected"),
+    (
+        ("this is a test", "tHiS Is a tEsT"),
+        ("THIS IS A TEST", "tHiS Is a tEsT"),
+        ("THe QuiCk FOX jumpED OVEr the LAzy DoG","tHe qUiCk fOx jUmPeD OvEr tHe lAzY DoG"),
+    )
+)
+
+def test_capitalise_letter(input_str, expected):
+    assert capitalize_letters(input_str)  ==  expected
+
+
